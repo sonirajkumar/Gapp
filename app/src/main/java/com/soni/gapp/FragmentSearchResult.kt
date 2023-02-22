92b7e8bdc89a735e3a7cfafaec0aecbb036737c4
raj <sonirajkumar76@yahoo.co.in>
2023-02-22 10:13:16 +0530

    removed lavender borders

--- app/src/main/java/com/soni/gapp/FragmentSearchResult.kt
+++ app/src/main/java/com/soni/gapp/FragmentSearchResult.kt
@@ -1,5 +1,6 @@
 package com.soni.gapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,26 +16,32 @@ class FragmentSearchResult : Fragment() {
     private lateinit var lName: String
     private lateinit var city: String
     private var mobileNumber: String? = null
-    private var aadharNumber: String? =null
-
+    private var aadharNumber: String? = null
+    private lateinit var documentId: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            fName = it.getString("f_name").toString()
-            mName = it.getString("m_name").toString()
-            lName = it.getString("l_name").toString()
-            city = it.getString("city").toString()
-            mobileNumber = it.getString("mobile_number").toString()
-            aadharNumber = it.getString("aadhar_number").toString()
+        arguments?.let { bundle ->
+            fName = bundle.getString("f_name").toString()
+            mName = bundle.getString("m_name").toString()
+            lName = bundle.getString("l_name").toString()
+            city = bundle.getString("city").toString()
+            mobileNumber = bundle.getString("mobile_number").toString()
+            aadharNumber = bundle.getString("aadhar_number").toString()
+            documentId = fName.filter { !it.isWhitespace() } +"_"+ mName.filter { !it.isWhitespace() } +"_"+ lName.filter { !it.isWhitespace() } +"_"+ city.filter { !it.isWhitespace() }+"_"+ mobileNumber!!.filter { !it.isWhitespace() }+"_"+ aadharNumber!!.filter { !it.isWhitespace() }
+
         }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSearchResultBinding.inflate(inflater, container, false)
-        binding.temptextview.text = "$fName $mName $lName $city $mobileNumber $aadharNumber"
+        val custDetails = "$fName $mName $lName $city"
+        binding.textViewSearchResultCustDetails.text = custDetails
+        binding.textViewMobileNumber.text = "Mobile: $mobileNumber"
+        binding.textViewAadharNumber.text = "Aadhar: $aadharNumber"
         return binding.root
     }
 }
