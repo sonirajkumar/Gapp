1daf6be2adf6618b4e3b3270721e0055626854b6
raj <sonirajkumar76@yahoo.co.in>
2023-02-22 15:33:30 +0530

    chagend project build config

--- app/src/main/java/com/soni/gapp/ActivityMain.kt
+++ app/src/main/java/com/soni/gapp/ActivityMain.kt
@@ -5,9 +5,6 @@ package com.soni.gapp
 //import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.soni.gapp.databinding.ActivityMainBinding
 
@@ -51,16 +48,4 @@ class ActivityMain : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
-    private var doubleBackToExitPressedOnce = false
-    override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
-            return
-        }
-
-        this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
-
-        Handler(Looper.getMainLooper()).postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
-    }
 }
