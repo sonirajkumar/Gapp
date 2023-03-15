29cd0f10b03ccb2e4826a463aba59d892ce1b78f
raj <sonirajkumar76@yahoo.co.in>
2023-03-15 05:53:53 +0530

    added removal of records greater than 20 in history

--- app/src/main/java/com/soni/gapp/FragmentMenu.kt
+++ app/src/main/java/com/soni/gapp/FragmentMenu.kt
@@ -15,7 +15,6 @@ import com.google.firebase.firestore.AggregateSource
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.soni.gapp.databinding.FragmentMenuBinding
-import com.soni.gapp.databinding.FragmentTransactionSearchBinding
 class FragmentMenu : Fragment() {
     private var _binding: FragmentMenuBinding? = null
     private val binding get() = _binding!!
@@ -39,11 +38,11 @@ class FragmentMenu : Fragment() {
     ): View {
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
         token = requireContext().getSharedPreferences("email", Context.MODE_PRIVATE)
-        val rvCustHistroy = binding.rvLastTransactions
-        rvCustHistroy.layoutManager = LinearLayoutManager(context)
+        val rvCustHistory = binding.rvLastTransactions
+        rvCustHistory.layoutManager = LinearLayoutManager(context)
         adapter = AdapterCustSearch(custSearchList)
         adapter.isTransferredFromHistory = true
-        rvCustHistroy.adapter = adapter
+        rvCustHistory.adapter = adapter
         custSearchList.clear()
         adapter.notifyDataSetChanged()
 
@@ -73,12 +72,19 @@ class FragmentMenu : Fragment() {
             db.collection("history").count().get(AggregateSource.SERVER)
                 .addOnCompleteListener {count->
                     if(count.isSuccessful){
-                        val countDiff = count.result.count - 10
-//                        if (countDiff>0){
-                            db.collection("history").orderBy("timestamp").get().addOnCompleteListener {
-                                println(it.result.documents)
-                            }
-//                        }
+                        val countDiff = count.result.count - 20
+                        if (countDiff>0) {
+                            db.collection("history").orderBy("timestamp").limit(countDiff)
+                                .get().addOnSuccessListener {
+                                    for (docs in it) {
+                                        println(docs.id)
+                                        db.collection("history").document(docs.id).delete()
+                                            .addOnSuccessListener {
+
+                                            }
+                                    }
+                                }
+                        }
                     }
                 }
         }
